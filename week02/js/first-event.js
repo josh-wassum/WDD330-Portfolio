a7efa97c3c5a3a52b463f21e9f303f4fcdf4af25
josh-wassum <joshuawassum@gmail.com>
2022-04-27 16:48:40 -0600

    Added Week 02 work

---
+++ week02/js/first-event.js
@@ -0,0 +1,6 @@
+// Main javascript file for week 2
+
+//  Sample code from Chapter 2: Programming Basics
+const question = "What is Superman's real name?"
+const answer = prompt(question);
+alert(`You answered ${answer}`);
